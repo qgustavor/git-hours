32a20a93ceb66f718d0e9fa29d70eed834c25a13
qgustavor <qgustavor@users.noreply.github.com>
2025-07-19 13:34:53 -0300

    Try a different approach to debug the issue in the CI

--- test/test-functional.js
+++ test/test-functional.js
@@ -9,7 +9,7 @@ const scriptPath = path.resolve(import.meta.dirname, '../src/index.js')
 describe('git-hours', () => {
   it('should output json', (t, done) => {
     execFile('node', [scriptPath], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.notEqual(work.total.hours.length, 0)
@@ -21,7 +21,7 @@ describe('git-hours', () => {
 
   it('Should analyse since today', (t, done) => {
     execFile('node', [scriptPath, '--since', 'today'], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.strictEqual(typeof work.total.hours, 'number')
@@ -31,7 +31,7 @@ describe('git-hours', () => {
 
   it('Should analyse since yesterday', (t, done) => {
     execFile('node', [scriptPath, '--since', 'yesterday'], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.strictEqual(typeof work.total.hours, 'number')
@@ -41,7 +41,7 @@ describe('git-hours', () => {
 
   it('Should analyse since last week', (t, done) => {
     execFile('node', [scriptPath, '--since', 'lastweek'], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.strictEqual(typeof work.total.hours, 'number')
@@ -51,7 +51,7 @@ describe('git-hours', () => {
 
   it('Should analyse since a specific date', (t, done) => {
     execFile('node', [scriptPath, '--since', '2015-01-01'], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.notEqual(work.total.hours, 0)
@@ -61,7 +61,7 @@ describe('git-hours', () => {
 
   it('Should analyse as without param', (t, done) => {
     execFile('node', [scriptPath, '--since', 'always'], (err, stdout, stderr) => {
-      if (err) console.error(stderr.toString())
+      if (err) err.stderr = stderr
       assert.ifError(err)
       const work = JSON.parse(stdout)
       assert.equal(work.total.hours, totalHoursCount)
